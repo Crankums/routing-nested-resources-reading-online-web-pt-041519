a99419a5213d62c26cddb9d3d57d9e90bd17c561
Craig Donavin <crdonavin@gmail.com>
2019-09-26 19:59:00 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
     resources :posts, only: [:show, :index]
   end
   
-  resources :posts, only: [:index, :show, :new, :create, :edit, :update]
+  resources :posts do
+    resources :comments
+  end
 
   root 'posts#index'
 end
